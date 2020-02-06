aa29dd6ef13fef28c5c49059fa4838602a08e7c5
ronnidc (RDC) <ronnidc@gmail.com>
2020-02-06 23:40:44 +0100

    Assets: Assets Management with Webpack

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from './kudo-logo.png';
+import Data from './data.xml';
+import JsonData from './data.json';
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('h1');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  // Add the image to our existing div.
+  const myLogo = new Image();
+  myLogo.src = Logo;
+
+  element.appendChild(myLogo);
+
+  console.log(Data);
+  console.log(JsonData);
 
   return element;
 }
-
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -14,6 +14,30 @@ module.exports = {
 			use: [
 					'style-loader',
 					'css-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader',
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader',
+				],
+			},
+			{
+				test: /\.(csv|tsv)$/,
+				use: [
+					'csv-loader',
+				],
+			},
+			{
+				test: /\.xml$/,
+				use: [
+					'xml-loader',
 				],
 			},
 		],

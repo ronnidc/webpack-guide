36eae2910b144dcc0a50a177df5ffcbaf0a67d30
ronnidc (RDC) <ronnidc@gmail.com>
2020-02-07 00:03:47 +0100

    Cleanup: Cleanup after Asset Management with Webpack

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Logo from './kudo-logo.png';
-import Data from './data.xml';
-import JsonData from './data.json';
 
 function component() {
-  const element = document.createElement('h1');
+  const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  // Add the image to our existing div.
-  const myLogo = new Image();
-  myLogo.src = Logo;
-
-  element.appendChild(myLogo);
-
-  console.log(Data);
-  console.log(JsonData);
 
   return element;
 }
